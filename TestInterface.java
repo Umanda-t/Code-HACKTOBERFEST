dec8821692d49f9e7db3984e53328c76fb0d7a5d
Galagamage Umanda Thathsarani <guthathsarani@students.nsbm.lk>
2020-10-01 21:45:24 -0700

    Create TestInterface.java

---
+++ TestInterface.java
@@ -0,0 +1,17 @@
+public class TestInterface {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {Speaker p= new Politician();
+        p.speak();
+     Speaker pr=new Priest();
+      pr.speak();
+         Speaker l=new Lecturer()
+      l.speak();
+      Works L=new Lecturer()
+      L.work();
+    }
+    
+}
