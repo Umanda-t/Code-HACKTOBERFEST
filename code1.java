c0ea2270fb17a2e72bf23e2628e39e92efbf47dc
Galagamage Umanda Thathsarani <guthathsarani@students.nsbm.lk>
2020-10-01 21:41:25 -0700

    Create code1.java

---
+++ code1.java
@@ -0,0 +1,12 @@
+public class Lecturer  implements Speaker,Works
+{
+public void speak()
+{
+System.out.println("Lecturer Talks Object Oriented Design and Programming!")
+}
+public void work()
+{
+System.out.println("Lecturer works at University")
+}
+
+}
