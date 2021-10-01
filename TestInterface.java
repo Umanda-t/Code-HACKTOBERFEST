db97e58e9f4bca16b71d74a965edc1c313ab438a
Galagamage Umanda Thathsarani <guthathsarani@students.nsbm.lk>
2021-10-01 14:03:08 +0530

    Update TestInterface.java
    update

--- TestInterface.java
+++ TestInterface.java
@@ -1,8 +1,5 @@
 public class TestInterface {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) 
     {Speaker p= new Politician();
         p.speak();
