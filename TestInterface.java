76da786f934da55ddfd9c42805110ceaaec35d08
Galagamage Umanda Thathsarani <guthathsarani@students.nsbm.lk>
2021-10-01 13:52:37 +0530

    Update TestInterface.java
    change

--- TestInterface.java
+++ TestInterface.java
@@ -4,10 +4,10 @@ public class TestInterface {
      * @param args the command line arguments
      */
     public static void main(String[] args) 
-    {Speaker p= new Politician();
-        p.speak();
-     Speaker pr=new Priest();
-      pr.speak();
+    {Speaker obj= new Politician();
+        obj.speak();
+     Speaker obj2=new Priest();
+      obj.speak();
          Speaker l=new Lecturer()
       l.speak();
       Works L=new Lecturer()
