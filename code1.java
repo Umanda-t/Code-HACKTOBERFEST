46fdaa1e1340c9cddea7f9616daf66408979c602
Galagamage Umanda Thathsarani <guthathsarani@students.nsbm.lk>
2021-10-01 14:54:30 +0530

    Update code1.java
    Update

--- code1.java
+++ code1.java
@@ -2,7 +2,7 @@ public class Lecturer  implements Speaker,Works
 {
 public void speak()
 {
-System.out.println("Lecturer Talks Object Oriented !")
+System.out.println("Talks Object Oriented !")
 }
 public void work()
 {
